f95a48a738e25282c4e934731e0f278081dd205b
tysiula <136995321+tysiula@users.noreply.github.com>
2024-02-02 14:15:37 +0100

    memory jq

--- memory.js
+++ memory.js
@@ -1,59 +1,72 @@
-for (let i=0; i<18; i++){
-    let div = document.createElement('div');
-    document.body.appendChild(div).setAttribute('class', 'div');}   //add divs
+$(document).ready(function () {
+    let cards, pairsGame;
+    let activeCard = '';
+    const active_cards = [];
+    let resultsGame = 0;
+    const startTime = new Date().getTime();
 
-const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
-let cards = document.getElementsByClassName('div');
-cards= [...cards];
+    addCardDivs();
+    init();
 
-const init = function () {
-    cards.forEach(x => {
-        const position = Math.floor(Math.random() * cardsColor.length);
-        x.classList.add(cardsColor[position]);
-        cardsColor.splice(position, 1);                                 // add div colorCards classes
-    })
-    setTimeout(function (){
-        cards.forEach(x => {
-            x.classList.add("hidden");
-            x.addEventListener("click", cardClick);
-        })
-    },2000)
-}
-init()                                                              // event click
-
-let activeCard = "";
-const active_cards= [];
-const pairsGame = cards.length / 2;
-let resultsGame = 0;
-
-const cardClick = function (){
-    activeCard = this;
-    activeCard.classList.remove("hidden");
-
-    if (active_cards.length === 0 ){
-        active_cards[0] = activeCard;
-        //return;
+    function addCardDivs() {
+        for (let i = 0; i < 18; i++) {
+            let div = document.createElement('div');
+            $(document.body).append($(div).attr('class', 'div'));
+        }
+        cards = $('.div');
+        cards = [...cards];
+        pairsGame = cards.length / 2;
     }
-    else {
-        cards.forEach(x => x.removeEventListener("click", cardClick))
-        active_cards[1] = activeCard;
 
-        setTimeout(function (){
-            if (active_cards[0].className === active_cards[1].className) {
-                active_cards.forEach(x => x.classList.add("off"))
-                resultsGame++;
+    function init() {
+        const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
+        addColorCardsToDiv();
+        setTimeout(function () {
+            cards.forEach(x => {
+                $(x).addClass("hidden")
+                x.addEventListener("click", cardClick);
+            })
+        }, 2000)
 
-                if (resultsGame === pairsGame) {
-                    alert('wygrana');
-                    //location.reload();
-                }
+        function addColorCardsToDiv() {
+            cards.forEach(x => {
+                const position = Math.floor(Math.random() * cardsColor.length);
+                $(x).addClass(cardsColor[position]);
+                cardsColor.splice(position, 1);
+            })
+        }
+    }
+
+    const cardClick = function () {
+        activeCard = this;
+        $(this).removeClass("hidden");
+
+
+        if (active_cards.length === 0) {
+            active_cards[0] = activeCard;
+        } else {
+            cards.forEach(x => x.removeEventListener("click", cardClick))
+            active_cards[1] = activeCard;
+
+            setTimeout(function () {
+                if (active_cards[0].className === active_cards[1].className) {
+                    active_cards.forEach(card => $(card).addClass("off"))
+                    resultsGame++;
+
+                    if (resultsGame === pairsGame) {
+                       const endTime = new Date().getTime();
+                       let gameTime = (endTime - startTime) / 1000;
+                       $('#message').html(
+                       'TWÓJ CZAS = ' +gameTime+ ' sekund' + '<br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>');
+                    }
                 } else {
-                    active_cards.forEach(x => x.classList.add("hidden"))
+                    active_cards.forEach(card => $(card).addClass("hidden"))
                 }
                 activeCard = "";
                 active_cards.length = 0;
                 cards.forEach(x => x.addEventListener("click", cardClick))
+            }, 1000)
+        }
+    };
 
-        }, 1000)
-    }
-};
+});
